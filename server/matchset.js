a334a1dad74133c739e6aae6ff5c93a5d499bce1
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2014-03-09 17:43:00 +0100

    Refactor.

--- server/matchset.js
+++ server/matchset.js
@@ -4,17 +4,17 @@ module.exports = MatchSet;
 function MatchSet () {
   var self = this;
 
-  self.players = { a: 0, b: 0 };
+  self.sides = { a: 0, b: 0 };
 
   self.point = function (side) {
-    self.players[side]++;
+    self.sides[side]++;
   };
 
   self.__defineGetter__("score", function () {
     // return copy
     return {
-      a: self.players.a,
-      b: self.players.b
+      a: self.sides.a,
+      b: self.sides.b
     };
   });
 
