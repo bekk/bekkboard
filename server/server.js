3ece822bd89ea64791ba0f1a4d9f31f6b07e20da
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2014-03-09 16:16:08 +0100

    Dummy event-emitter for server.

--- server/server.js
+++ server/server.js
@@ -1,2 +1,6 @@
-var app = require('./app');
+// TODO trondkla geirsagberg bytt ut denne med deres
+// var events = require('ble-event-module');
+var events = new process.EventEmitter();
+
+var app = require('./app')(events);
 app.listen(3000);
