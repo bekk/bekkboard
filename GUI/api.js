2aaa38872566a37aa64487eb1bbc884b64a9eea7
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2014-09-21 19:21:25 +0200

    Fix json communicaiton.

--- GUI/api.js
+++ GUI/api.js
@@ -2,7 +2,14 @@ var Api = (function () {
   var url = "http://localhost:3000";
   return {
     signUp: function (name, fn) {
-      $.post(url + "/signup", { name: name }, fn);
+      $.ajax({
+        type: "post",
+        url: url + "/signup",
+        data: JSON.stringify({ name: name }),
+        contentType: 'application/json',
+        dataType: 'json',
+        success: fn
+      });
     },
     getStatus: function (fn) {
       $.get(url + "/status", fn);
