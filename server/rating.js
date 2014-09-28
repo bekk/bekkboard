68d498402af25dd74dab328fac73cfe14e6220dc
Eirik J. Daling <eirik@daling.net>
2014-09-28 15:59:32 +0200

    Added module will calculate rating

---
+++ server/rating.js
@@ -0,0 +1,22 @@
+var getRating = function(){
+	return [
+	{
+		player: {
+			name: "Eirik",
+			number: "1234"
+		},
+		rating: 1234
+	},
+	{
+		player: {
+			name: "torgeir",
+			number: "666"
+		},
+		rating: 1000
+	}
+	];
+};
+
+module.exports = {
+	getRating: getRating
+}
