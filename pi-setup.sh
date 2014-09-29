4a2876d612b6d81792450c44b7058df2be18b133
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2014-09-29 13:36:47 +0200

    Adds pi-setup.sh to install needed raspbian packages.

---
+++ pi-setup.sh
@@ -0,0 +1,11 @@
+# chmod u+x pi-setup.sh
+
+# useful packages, needed to run bekkboard
+sudo apt-get install \
+  chromium-browser \
+  ttf-mscorefonts-installer \
+  git \
+  vim \
+  build-essential \
+  openssl \
+  libssl-dev
