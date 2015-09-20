5e2d4d17e9a0b36424b0f5eba342deff3319ceb3
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-09-20 15:39:12 +0200

    Check data.sets for undefined aswell

--- GUI/resources/js/pages/score.js
+++ GUI/resources/js/pages/score.js
@@ -32,7 +32,7 @@ module.exports = Ractive.extend({
       self.set('draw', data.draw);
       self.set('time', data.time);
       self.set('showscore', data && (data.status === 'ready' || data.status === 'started' || data.status === 'stopped'));
-      self.set('vsinvertedclass', data && ((data.sets.a + data.sets.b) % 2 === 0 ? '' : 'vs-score-inverted'));
+      self.set('vsinvertedclass', data && data.sets && ((data.sets.a + data.sets.b) % 2 === 0 ? '' : 'vs-score-inverted'));
       self.set('playeraclass', data && (data.servingPlayer === 'a' ? 'serving-player' : ''));
       self.set('playerbclass', data && (data.servingPlayer === 'b' ? 'serving-player' : ''));
     });
