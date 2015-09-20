22527ed8c8323e4b2b5eb935f564b3fb8143ea65
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2015-09-20 13:39:41 +0200

    Idempotent pi-setup

--- pi-setup.sh
+++ pi-setup.sh
@@ -35,19 +35,48 @@ chmod u+x /home/pi/bekkboard.git/hooks/post-receive
 cd
 
 # install node
+  # to be able to send f11 to the browser to make it go fullscreen
+
+sudo apt-get install upstart --force-yes
+
+# go to the home folder
+cd
+
+# create a folder well host the bare repo in
+mkdir bekkboard.git && cd bekkboard.git
+git init --bare
+ln -s /home/pi/bekkboard/pi/post-receive /home/pi/bekkboard.git/hooks/post-receive
+chmod u+x /home/pi/bekkboard.git/hooks/post-receive
+
+# on you personal computer, add a remote to it, and push away!
+# git remote add pi pi@bekkpi2.local:~/bekkboard.git
+# git push pi master
+
+# go to the home folder
+cd
+
+# install node
+rm node-v4.1.0-linux-armv7l.tar.gz
+rm -rf node-v4.1.0-linux-armv7l
+
 wget https://nodejs.org/dist/v4.1.0/node-v4.1.0-linux-armv7l.tar.gz
 tar xvfz node-v4.1.0-linux-armv7l.tar.gz
 cd node-v4.1.0-linux-armv7
 sudo cp -R * /usr/local/
 
 # install launch-on-boot scripts
+rm -rf /etc/init/bekkboard-api.conf
+rm -rf /etc/init/bekkboard-gui.conf
+
 cd /etc/init/
 sudo ln -s /home/pi/bekkboard/pi/bekkboard-api.conf
 sudo ln -s /home/pi/bekkboard/pi/bekkboard-gui.conf
 
 # install start as kiosk mode
+rm -rf /etc/xdg/lxsession/LXDE/autostart
 cd /etc/xdg/lxsession/LXDE/
 sudo ln -sf /home/pi/bekkboard/pi/autostart
 
 # install shortcut to starting the browser, launched by the autostart file
+rm -rf /usr/bin/launch-bekkboard-browser
 sudo ln -s /home/pi/bekkboard/pi/launch-bekkboard-browser /usr/bin/launch-bekkboard-browser
