7c78994518a710f56a437fabd272d0d787f65438
Alexander Hanssen <alexhsn1@gmail.com>
2015-11-06 11:36:15 +0100

    Adds popping number when a player gets the point

--- GUI/resources/js/pages/score.js
+++ GUI/resources/js/pages/score.js
@@ -17,22 +17,26 @@ module.exports = Ractive.extend({
     palyeraclass: '',
     playerbclass: '',
     vsinvertedclass: '',
-    ranking: []
+    ranking: [],
+    animateA: '',
+    animateB: ''
   },
 
   init: function () {
     var self = this;
     ES.on('score', function (data) {
+      self.set('animateA', data && (self.data.score.a !== data.score.a && data.score.a !== 0 ? 'trigger-animate' : ''));
+      self.set('animateB', data && (self.data.score.b !== data.score.b && data.score.b !== 0 ? 'trigger-animate' : ''));
+      self.set('vsinvertedclass', data && data.sets && ((data.sets.a + data.sets.b) % 2 === 0 ? '' : 'vs-score-inverted'));
+      self.set('playeraclass', data && (data.servingPlayer === 'a' ? 'serving-player' : ''));
+      self.set('playerbclass', data && (data.servingPlayer === 'b' ? 'serving-player' : ''));
+      self.set('showscore', data && (data.status === 'ready' || data.status === 'started' || data.status === 'stopped'));
       self.set('score', data.score);
       self.set('sets', data.sets);
       self.set('winner', data.winner);
       self.set('players', data.players);
       self.set('draw', data.draw);
       self.set('time', data.time);
-      self.set('showscore', data && (data.status === 'ready' || data.status === 'started' || data.status === 'stopped'));
-      self.set('vsinvertedclass', data && data.sets && ((data.sets.a + data.sets.b) % 2 === 0 ? '' : 'vs-score-inverted'));
-      self.set('playeraclass', data && (data.servingPlayer === 'a' ? 'serving-player' : ''));
-      self.set('playerbclass', data && (data.servingPlayer === 'b' ? 'serving-player' : ''));
     });
 
     ES.on('ranking', function (ranking) {
